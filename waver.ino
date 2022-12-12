25ad2a60b989f84de4b761d0031487d75e023c27
Sergey Kovalev <xray@randoman.ru>
2022-12-13 01:23:15 +0700

    Improve effects

--- waver.ino
+++ waver.ino
@@ -12,7 +12,6 @@ uint8_t storage[STRIP_NUMLEDS];
 Canvas  canvas(storage, STRIP_NUMLEDS, &strip);
 FXChain fx_chain;
 
-
 void setup() {
   Serial.begin(115200);
   canvas.Fill(0);
@@ -21,20 +20,16 @@ void setup() {
   int middle = size / 2 - 1;
 
 
-  fx_chain.append(new CFront(&canvas, 0, size - 1, 7, false, 3, 2, 0, 192));
-//  fx_chain.append(new CFront(&canvas, -5, size - 6, 7, false, 3, -2, 0, 0));
-  fx_chain.append(new CFront(&canvas, size - 1, 0, 7, false, 3, 2, 0, 192));
-//  fx_chain.append(new CFront(&canvas, size + 4, 5, 7, false, 3, -2, 0, 0));
-  fx_chain.append(new CFade(&canvas, 0, 1));
+  fx_chain.append(new CFront(&canvas, 0, size - 1, 7, false, 3, 10, 0, 192));
+  fx_chain.append(new CFront(&canvas, size - 1, 0, 7, false, 3, 10, 0, 192));
+  fx_chain.append(new CFade(&canvas, 0, 4));
   fx_chain.append_next_chain();
-  fx_chain.append(new CFront(&canvas, 0, middle, 7, false, 3, 2, 0, 192));
-//  fx_chain.append(new CFront(&canvas, -5, middle - 5, 7, false, 3, -2, 0, 0));
-  fx_chain.append(new CFront(&canvas, size - 1, middle, 7, false, 3, 2, 0, 192));
-//  fx_chain.append(new CFront(&canvas, size + 4, middle +5, 7, false, 3, -2, 0, 0));
-  fx_chain.append(new CFade(&canvas, 0, 1));
+  fx_chain.append(new CFront(&canvas, 0, middle + 4, 7, false, 3, 10, 0, 192));
+  fx_chain.append(new CFront(&canvas, size - 1, middle - 3, 7, false, 3, 10, 0, 192));
+  fx_chain.append(new CFade(&canvas, 0, 4));
   fx_chain.append_next_chain();
-  fx_chain.append(new CFront(&canvas, middle + 1, size - 1, 7, false, 5, 2, 0, 192));
-  fx_chain.append(new CFront(&canvas, middle, 0, 7, false, 5, 2, 0, 192));
+  fx_chain.append(new CFront(&canvas, middle + 4, size - 1, 7, false, 5, 2, 0, 192));
+  fx_chain.append(new CFront(&canvas, middle - 3, 0, 7, false, 5, 2, 0, 192));
   fx_chain.append_next_chain();
   fx_chain.append(new CFront(&canvas, middle + 1 - 2, size + 1, 7, false, 5, -2, 0, 0));
   fx_chain.append(new CFront(&canvas, middle + 2, -2, 7, false, 5, -2, 0, 0));
